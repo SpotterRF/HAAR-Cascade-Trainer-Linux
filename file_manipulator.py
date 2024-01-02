0c9a8347c87c63c06cc810b7226512f4f3600158
miki-spotter <miki.jauhiainen@spotterglobal.com>
2024-01-02 13:08:46 -0700

    Add pictures of cars from behind

--- file_manipulator.py
+++ file_manipulator.py
@@ -7,18 +7,18 @@ def read_files(input_dir):
 
 def write_files(input_dir, output_dir, files):
     for i, image in enumerate(files):
-        # shutil.copy("{}/{}".format(input_dir, image), "{}pos271223-{}.png".format(output_dir, i))
-        shutil.copy("{}/{}".format(input_dir, image), "{}neg281223-{}.png".format(output_dir, i))
+        shutil.copy("{}/{}".format(input_dir, image), "{}pos020124-{}.png".format(output_dir, i))
+        # shutil.copy("{}/{}".format(input_dir, image), "{}neg281224-{}.png".format(output_dir, i))
 
 
 if __name__ == "__main__":
     print(os.getcwd())
-    # input_dir = "../../Pictures/Training images/Cars"
-    # output_dir = "data/car/p/"
+    input_dir = "../../Pictures/New"
+    output_dir = "data/car/p/"
     # input_dir = "../../Pictures/Training images/People"
     # output_dir = "data/people/p/"
-    input_dir = "../../Pictures/Training images/Roads - People"
-    output_dir = "data/people/n/"
+    # input_dir = "../../Pictures/Training images/Roads - People"
+    # output_dir = "data/people/n/"
 
     files = read_files(input_dir)
     write_files(input_dir, output_dir, files)
